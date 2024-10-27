f6e3fa068cdd4ffe81c0607ae7c771b014613550
Pacalini <141402887+Pacalini@users.noreply.github.com>
2024-10-27 22:26:39 +0800

    ehentai: fix cookies validation

--- ehentai.js
+++ ehentai.js
@@ -81,6 +81,12 @@ class Ehentai extends ComicSource {
              * @returns {Promise<boolean>}
              */
             validate: async (values) => {
+                if (values.length !== 4) {
+                    return false
+                }
+                if (values[0].length === 0 || values[1].length === 0) {
+                    return false
+                }
                 let cookies = []
                 for (let i = 0; i < values.length; i++) {
                     cookies.push(new Cookie({
@@ -95,12 +101,6 @@ class Ehentai extends ComicSource {
                     }))
                 }
                 Network.setCookies('https://e-hentai.org', cookies)
-                if (cookies.length !== 4) {
-                    return false
-                }
-                if (cookies[0].length === 0 || cookies[1].length === 0) {
-                    return false
-                }
                 let res = await Network.get(
                     "https://forums.e-hentai.org/",
                     {
